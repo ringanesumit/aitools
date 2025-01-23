45c66175ea5fe518d096ef2102127d955aa76b6a
User <sumitringane@outlook.com>
2025-01-23 23:53:39 +0530

    first vercelv2

---
+++ src/AITools/popUp.js
@@ -0,0 +1,193 @@
+const contentData = {
+    aboutUs: `
+  <div class="ai-tools-description">
+  <h2>Welcome to the AI Tools Directory</h2>
+  <p>
+    The <strong>AI Tools Directory</strong> is directory serves as a gateway to innovation, providing solutions 
+    that enhance creativity, optimize workflows, and boost productivity.
+  </p>
+  <p>
+    Explore a wide range of tools designed to boost creativity, streamline workflows, and maximize productivity. Whether you’re looking to:
+  </p>
+  <ul>
+    <li>Create stunning AI-generated art</li>
+    <li>Enhance photo quality and restore old images</li>
+    <li>Generate high-quality content for websites, blogs, and sales pages</li>
+    <li>Compose unique music and soundscapes</li>
+    <li>Automate repetitive tasks and data entry</li>
+    <li>Integrate AI assistants into your workflow for scheduling and email management</li>
+    <li>Analyze data for marketing insights and business growth</li>
+    <li>Improve customer engagement with AI-powered chatbots</li>
+    <li>Translate content and documents with advanced AI language tools</li>
+    <li>Develop creative video content with AI-based video editing tools</li>
+    <li>Generate research summaries and automate citation management</li>
+    <li>Optimize e-commerce strategies with AI-driven product recommendations</li>
+    <li>Create custom branding materials like logos and taglines</li>
+  </ul>
+  <p>
+    This directory has everything you need to transform your creative endeavors, optimize business processes, and unlock the full potential of AI. 
+  </p>
+  <p>
+    <em>Discover how artificial intelligence can elevate your productivity and drive innovation today!</em>
+  </p>
+</div>
+
+    `,
+    privacyPolicy: `
+  <div class="ai-tools-privacyPolicy">
+  <h2>Privacy Policy</h2>
+  <p>
+    Welcome to the <strong>AI Tool Directory Privacy Policy</strong>. This policy outlines how <strong>AI Tool Directory</strong> ("Company," "we," "us," or "our") collects, uses, discloses, and protects your personal information when you use the <strong>AI Tool Directory Website</strong> ("Website"). This policy complies with the <strong>General Data Protection Regulation (GDPR)</strong> and other applicable data protection laws.
+  </p>
+
+  <h3>Data Collection and Usage</h3>
+  <p>We collect and process personal information in the following ways:</p>
+  <ul>
+    <li><strong>Information You Provide:</strong> Personal details you share via forms, surveys, or other means (e.g., name, email address).</li>
+    <li><strong>Usage Data:</strong> Automatically collected information about your interaction with the Website, such as IP address, browser type, and visit timestamps.</li>
+    <li><strong>Cookies and Tracking Technologies:</strong> Data collected using cookies to enhance your browsing experience and track preferences.</li>
+  </ul>
+
+  <h3>Purpose and Legal Basis of Data Processing</h3>
+  <p>We process personal data for the following purposes:</p>
+  <ul>
+    <li><strong>Service Improvement:</strong> Operate, maintain, and enhance the Website’s functionality.</li>
+    <li><strong>Personalized Experience:</strong> Tailor content and services to match your preferences.</li>
+    <li><strong>Promotional Communications:</strong> With your consent, send newsletters or promotional emails.</li>
+    <li><strong>Legal Obligations:</strong> Fulfill legal requirements and comply with regulations.</li>
+  </ul>
+
+  <h3>Data Sharing and Disclosure</h3>
+  <p>We may share your personal information in these cases:</p>
+  <ul>
+    <li><strong>Service Providers:</strong> Engage trusted third parties to assist with Website functionality and services.</li>
+    <li><strong>Legal Requirements:</strong> Comply with laws or respond to legal requests.</li>
+    <li><strong>Business Transfers:</strong> Share information during mergers, acquisitions, or asset sales.</li>
+  </ul>
+
+  <h3>Data Retention</h3>
+  <p>We retain personal information as long as necessary for outlined purposes or as required by law. Once no longer needed, we securely delete or anonymize your data.</p>
+
+  <h3>Data Security</h3>
+  <p>
+    We use appropriate technical and organizational measures to protect your information. However, no method of transmission or storage is 100% secure.
+  </p>
+
+  <h3>Your Rights</h3>
+  <p>
+    Under GDPR, you have rights such as access, rectification, erasure, and objection to data processing. To exercise these rights, contact us using the details below.
+  </p>
+
+  <h3>Third-Party Websites</h3>
+  <p>
+    The Website may link to third-party websites not operated by us. We encourage you to review their privacy policies before sharing personal data.
+  </p>
+
+  <h3>Changes to This Policy</h3>
+  <p>
+    We may update this Policy to reflect changes in practices or legal requirements. Continued use of the Website after updates signifies your acceptance.
+  </p>
+
+  <h3>Contact Us</h3>
+  <p>
+    If you have any questions about this Policy, please reach out:
+  </p>
+  <address>
+    <strong>AI Tool Directory</strong><br>
+    sumitringane@outlook.com
+  </address>
+</div>
+
+    `,
+    termsOfUse: `
+  <div class="terms-of-use">
+  <h2>Welcome to AI Tool Directory's Terms of Use</h2>
+  <p>
+    The <strong>AI Tool Directory Website</strong> offers a platform for discovering and exploring various artificial intelligence software solutions. These Terms govern your access and use of the Website and the services provided therein.
+  </p>
+  <p>
+    By accessing or using the Website, you agree to comply with and be bound by these Terms. If you do not agree with any part of these Terms, please refrain from using the Website.
+  </p>
+
+  <h3>Description of Service</h3>
+  <p>
+    <strong>AI Tool Directory</strong> provides a free online directory that helps users search, explore, and learn about various AI tools, featuring details like their functionalities, reviews, and other relevant information.
+  </p>
+
+  <h3>Monetization</h3>
+  <p>
+    We reserve the right to monetize the Website through various methods, such as advertisements, affiliate links, sponsorships, and subscriptions. These monetization strategies do not imply endorsement or recommendation of any third-party products or services.
+  </p>
+
+  <h3>Accuracy and Liability Disclaimer</h3>
+  <p>
+    The Company makes no representations about the accuracy, completeness, or timeliness of the content provided. The information is for general purposes and should not be interpreted as professional or legal advice. Use of the Website and reliance on the information provided is at your own risk.
+  </p>
+
+  <h3>Limitation of Liability</h3>
+  <p>
+    To the fullest extent permitted by applicable law, we are not liable for any direct, indirect, incidental, or consequential damages resulting from the use or inability to use the Website, including loss of profits or data.
+  </p>
+
+  <h3>Intellectual Property</h3>
+  <p>
+    All content on the Website, including design, graphics, logos, trademarks, and other intellectual property, is protected by applicable laws and is owned by us or third parties. Reproduction or use of any content without written consent is prohibited.
+  </p>
+
+  <h3>Website Use Guidelines</h3>
+  <p>
+    By using the Website, you agree to the following:
+  </p>
+  <ul>
+    <li>Do not disrupt or interfere with the Website’s operation or security.</li>
+    <li>Avoid using the Website for illegal, harmful, or unauthorized purposes.</li>
+    <li>Do not collect personal information from other users without their consent.</li>
+    <li>Do not impersonate others or misrepresent affiliations.</li>
+    <li>Refrain from uploading or transmitting unlawful or harmful content.</li>
+    <li>Do not attempt unauthorized access to any parts of the Website.</li>
+    <li>Do not scrape or extract data without prior written consent.</li>
+  </ul>
+
+  <h3>Governing Law</h3>
+  <p>
+    These Terms are governed by the laws of England and Wales, without regard to its conflict of laws principles.
+  </p>
+
+  <h3>Contact Information</h3>
+  <p>
+    If you have any questions regarding these Terms or the Website, please reach out to us at:
+  </p>
+  <address>
+    sumitringane@outlook.com
+  </address>
+  <p>
+    <em>We encourage you to review these Terms regularly for updates and changes.</em>
+  </p>
+</div>
+    `,
+    contactUs: `
+      <div class="contact-us-container">      
+    <h1>Contact Us</h1>
+    <p>
+        We’d love to hear from you! <br>Whether you have a question about our services, need assistance, or want to 
+        provide feedback, feel free to reach out to us through the following channels.
+      </p>
+    <p>Have a question or comment? We'd love to hear from you.</p>
+    <div class="contact-info">
+  <div class="contact-container">
+    <p>Have a question or comment? We'd love to hear from you.</p>
+    <form class="contact-form">
+      <div class="form-row">
+        <input type="text" placeholder="Subject" required>
+        <input type="email" placeholder="Your email address" required>
+      </div>
+      <input type="text" placeholder="Your Name" required>
+      <textarea placeholder="Your message" rows="5" required></textarea>
+      <button type="submit">Contact us</button>
+    </form>
+  </div>
+</div>
+  </div>
+    `,
+  };
+  export default contentData;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AItoolsData from "./AITools/toolsData";
-//import {AboutUs} from "./logo/about-us-placeholder.png";
+import contentData from "./AITools/popUp";
+import Select from "react-select";
 import "./App.css";
 
 const toolsData = AItoolsData;
 
 const App = () => {
-  const [view, setView] = useState("home"); // Possible values: 'home', 'tools', 'contact', 'about'
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("all");
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
   const [currentPage, setCurrentPage] = useState(1);
+
+  // Theme toggle state
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Toggle theme function
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.style.backgroundColor = isDarkMode ? "white" : "black";
+    document.body.style.color = isDarkMode ? "black" : "white";
+  };
+
+  // PopUp Data
+  const [popupTitle, setPopupTitle] = useState("");
+  const [popupContent, setPopupContent] = useState("");
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  // Functions to show and hide the popup
+  const showPopup = (title, content) => {
+    setPopupTitle(title);
+    setPopupContent(content);
+    setIsPopupVisible(true);
+  };
+
+  const hidePopup = () => {
+    setIsPopupVisible(false);
+    setPopupTitle("");
+    setPopupContent("");
+  };
 
   // Filter tools based on search and category
   const filteredTools = toolsData.filter(
@@ -18,6 +46,11 @@ const App = () => {
       (category === "all" || tool.category === category) &&
       tool.name.toLowerCase().includes(search.toLowerCase())
   );
+
+  // Reset currentPage if filters change
+  useEffect(() => {
+    setCurrentPage(1); // Reset to page 1 when filters change
+  }, [search, category]);
 
   // Pagination calculations
   const totalPages = Math.ceil(filteredTools.length / itemsPerPage);
@@ -31,145 +64,97 @@ const App = () => {
   const handlePageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
       setCurrentPage(newPage);
+
+      // Smooth scroll to the top
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     }
   };
 
   // Get unique categories from toolsData
   const categories = ["all", ...new Set(toolsData.map((tool) => tool.category))];
+  const categoryOptions = categories.map((cat) => ({
+    value: cat,
+    label: cat === "all" ? "All Categories" : cat,
+  }));
+
+  // Handle Select onChange to update the category state
+  const handleCategoryChange = (selectedOption) => {
+    setCategory(selectedOption ? selectedOption.value : "all");
+  };
+
+  // Count total tools
+  const totalToolsCount = toolsData.length;
+  const categoryCount = category === "all" 
+    ? totalToolsCount
+    : filteredTools.length;
 
   return (
-    <div className="app">
-      <header>
+    <div className={`app ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+      <header className="header">
         <h1>AI Tools Directory</h1>
-        <nav>
-          <ul className="menu">
-            <li>
-              <a href="#home" onClick={() => setView("home")}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#about" onClick={() => setView("about")}>
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#tools" onClick={() => setView("tools")}>
-                Tools
-              </a>
-            </li>
-            <li>
-              <a href="#contact" onClick={() => setView("contact")}>
-                Contact
-              </a>
-            </li>            
-          </ul>
-        </nav>
+        <div
+          className="theme-toggle"
+          onClick={toggleTheme}
+          style={{
+            cursor: "pointer",
+            fontSize: "24px",
+            padding: "10px",
+            borderRadius: "50%",
+            background: isDarkMode ? "#333" : "#eee",
+            color: isDarkMode ? "white" : "black",
+          }}
+        >
+          {isDarkMode ? "🌙" : "☀️"}
+        </div>
       </header>
 
-      <main>
-        {view === "home" && (
-          <div class="home">
-          <div class="home-content">
-            <h2>Welcome to AI Tools Directory!</h2>
-            <p>
-              Explore the best AI tools that can transform your workflow. Whether you're a 
-              developer, designer, data scientist, or business owner, we provide an extensive 
-              collection of the latest AI technologies to enhance creativity, automate tasks, 
-              and make smarter decisions.
-            </p>
-            <h3>Why AI Tools Matter?</h3>
-            <ul>
-              <li><strong>Boost Productivity:</strong> Automate repetitive tasks and save time.</li>
-              <li><strong>Enhance Creativity:</strong> Brainstorm, generate content, and create stunning visuals.</li>
-              <li><strong>Smarter Decisions:</strong> Use data-driven insights for better business decisions.</li>
-              <li><strong>Seamless Integration:</strong> Discover tools that easily integrate with your workflow.</li>
-            </ul>
-            <p>Start exploring now to discover the tools that best fit your needs!</p>
-          </div>
-          </div>
-        
-        )}
-
-        {view === "contact" && (
-          <div class="contact">
-          <div class="contact-content">
-            <h2>Contact Us</h2>
-            <p>
-              We'd love to hear from you! If you have questions, suggestions, or need assistance,
-              feel free to reach out to us. Our team is here to help you explore the best AI tools.
-            </p>
-            <h3>Get in Touch</h3>
-            <ul>
-              <li><strong>Email:</strong> sumitringane@outlook.com</li>
-              <li><strong>Address:</strong> IT Park Parsodi, Nagpur,MH,India</li>
-            </ul>
-            <h3>Stay Connected</h3>
-              <p>Follow us on social media to get the latest updates on AI tools and technologies:</p>
-              <ul class="social-links">
-                <li><a href="https://www.linkedin.com/in/sumit-ringane-1990/" target="_blank">LinkedIn</a></li>
-              </ul>
+      <section className="hero">
+        <div className="filters">
+          <input
+            type="text"
+            placeholder="Search for a tool..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className="category-filter">
+            <Select
+              options={categoryOptions}
+              value={categoryOptions.find((option) => option.value === category) || null}
+              onChange={handleCategoryChange}
+              placeholder="Select Category"
+              isClearable
+              styles={{
+                control: (provided) => ({
+                  ...provided,
+                  width: '300px',  // Set dropdown width to 300px
+                }),
+                singleValue: (provided) => ({
+                  ...provided,
+                  color: 'black',  // Set color of the selected value to black
+                }),
+                option: (provided) => ({
+                  ...provided,
+                  color: 'black',  // Set color of the options to black
+                }),
+                menu: (provided) => ({
+                  ...provided,
+                  color: 'black',  // Set color of the dropdown menu to black
+                }),
+              }}
+            />            
           </div>          
-        </div>        
-        )}
-        {view === "about" && (
-          <div class="home">
-          <div class="home-content">
-            <h2>About Us</h2>
-            <p>
-              Welcome to <strong>AI Tools Directory</strong>, your one-stop destination for discovering the best 
-              AI-powered tools. Our mission is to empower developers, creators, and businesses by 
-              providing access to innovative tools that drive productivity, creativity, and growth.
-            </p>
-            <p>
-              At <strong>AI Tools Directory</strong>, we believe in the transformative power of artificial 
-              intelligence. From automation to analytics, design to development, we curate tools 
-              that bring cutting-edge AI technologies closer to you. We aim to bridge the gap between 
-              technology and users, making AI accessible and easy to integrate into everyday workflows.
-            </p>
-            <h3>Why Choose Us?</h3>
-            <ul>
-              <li><strong>Comprehensive Directory:</strong> Explore tools across diverse categories and industries.</li>
-              <li><strong>User-Centric:</strong> Tailored to suit everyone, from beginners to advanced users.</li>
-              <li><strong>Trusted Recommendations:</strong> Only the best AI tools make it to our directory.</li>
-              <li><strong>Constant Updates:</strong> Stay ahead with the latest trends in AI technology.</li>
-            </ul>
-            <p>
-              Join our journey as we navigate the exciting world of AI and uncover tools that 
-              redefine what’s possible. Together, let’s shape the future of work, creativity, and innovation!
-            </p>
-          </div>
-          </div>    
-        )}
+          <span className="tool-count">
+              {category === "all" ? `${totalToolsCount} results` : `${categoryCount} results`}
+            </span>
+        </div>
+      </section>
 
-        {view === "tools" && (
-          <div>
-            <div className="filters">
-              <input
-                type="text"
-                placeholder="Search for a tool..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <select
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-              >
-                {categories.map((cat) => (
-                  <option key={cat} value={cat}>
-                    {cat === "all" ? "All Categories" : cat}
-                  </option>
-                ))}
-              </select>
-              <select
-                value={itemsPerPage}
-                onChange={(e) => setItemsPerPage(parseInt(e.target.value))}
-              >
-                <option value={10}>10</option>
-                <option value={20}>20</option>
-                <option value={30}>30</option>
-              </select>
-            </div>
+      <section id="features" className="features">
+        <div>
+          <main>
             <div className="tool-grid">
               {displayedTools.map((tool) => (
                 <div key={tool.name} className="tool-card">
@@ -177,7 +162,7 @@ const App = () => {
                     src={require("./logo/" + tool.icon)}
                     alt={`${tool.name} icon`}
                   />
-                  <h3>{tool.name}</h3>
+                  <h3 className="toolname">{tool.name}</h3>
                   <p>{tool.description}</p>
                   <a
                     href={tool.link}
@@ -189,12 +174,9 @@ const App = () => {
                 </div>
               ))}
             </div>
-            
-          </div>
-        )}
-      </main>
-      {view === "tools" && (
-      <div className="pagination">
+          </main>
+          {filteredTools.length > 0 && (
+            <div className="pagination">
               <button
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={currentPage === 1}
@@ -210,10 +192,72 @@ const App = () => {
               >
                 Next
               </button>
-        </div>)}
-      <footer>
-        <p>&copy; {new Date().getFullYear()} AI Tools Directory. All rights reserved.</p>
+            </div>
+          )}
+          {filteredTools.length === 0 && (
+            <div>No results found</div>
+          )}
+        </div>
+      </section>
+
+      <section className="cta">
+        <h3>Join the AI Revolution</h3>
+        <p>Sign up now to access the latest AI tools and stay ahead of the curve.</p>
+        <button>Get Started</button>
+      </section>
+
+      <footer className="footer">
+        <div className="footer-menu">
+          <button
+            onClick={() =>
+              showPopup("About Us", contentData.aboutUs)
+            }
+          >
+            About Us
+          </button>
+          <button
+            onClick={() =>
+              showPopup("Privacy Policy", contentData.privacyPolicy)
+            }
+          >
+            Privacy Policy
+          </button>
+          <button
+            onClick={() =>
+              showPopup("Terms of Use", contentData.termsOfUse)
+            }
+          >
+            Terms of Use
+          </button>
+          <button
+            onClick={() =>
+              showPopup("Contact Us", contentData.contactUs)
+            }
+          >
+            Contact Us
+          </button>
+        </div>
+        <div className="footer-right">
+          <p>© 2025 AIToolDirectory.V2</p>
+        </div>
       </footer>
+
+      {/* Overlay and Popup */}
+      {isPopupVisible && (
+        <>
+          <div className="overlay" onClick={hidePopup}></div>
+          <div className="popup">
+            <div className="popup-header">{popupTitle}</div>
+            <div
+              className="popup-content"
+              dangerouslySetInnerHTML={{ __html: popupContent }}
+            ></div>
+            <button className="popup-close" onClick={hidePopup}>
+              Close
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
